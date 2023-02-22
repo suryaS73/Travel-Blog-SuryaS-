eae3f0783e6460282520a9770878f9d67075c021
suryaS73 <125781536+suryaS73@users.noreply.github.com>
2023-02-22 13:31:05 +0530

    Add files via upload

---
+++ Java Script/Gallery.js
@@ -0,0 +1,22 @@
+function colo()
+{
+    var col=["tiger.jpg","butterfly.jpg","lion.jpg","parrot.jpg","giraffe.jpg","swan.jpg","lotus.jpg","fish.jpg"]
+    var len=col.length
+    var ran=Math.random()*len
+    var rannum=Math.round(ran)
+    document.getElementById("a").style.backgroundImage="URL(../Assets/Images/"+col[rannum]+")"
+    var updatethetime=setTimeout(function(){colo()},2000)
+}
+// from Here Gallary Code Starts
+function imgK(pic,heading,discription)
+{
+    document.getElementById("bigscreen").removeAttribute("style")
+    document.getElementById("imgheading").innerHTML=heading;
+    document.getElementById("imgparagraph").innerHTML=discription;
+    bigscreen.style.display="flex"
+    fullImg.src=pic;
+}
+function imgV()
+{
+    document.getElementById("bigscreen").setAttribute("style","display:none")
+}
